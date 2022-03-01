3b9434ee1bb88a0672b0ff8baf9cd58046e7ddc1
Adam Lee <s016651@students.lmsd.org>
2022-03-01 13:27:43 -0500

    Initial Commit

---
+++ MyGraine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyGraine
+//
+//  Created by Adam Lee (student LM) on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyGraine/MyGraineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGraineApp.swift
+//  MyGraine
+//
+//  Created by Adam Lee (student LM) on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MyGraineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
