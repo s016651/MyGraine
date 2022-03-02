a520f66bf89308a3ca34048994e3099b2ea20c2a
Adam Lee <s016651@students.lmsd.org>
2022-03-02 14:02:23 -0500

    added code for fire base

--- MyGraine/MyGraineApp.swift
+++ MyGraine/MyGraineApp.swift
@@ -6,7 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
 
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 @main
 struct MyGraineApp: App {
     var body: some Scene {
