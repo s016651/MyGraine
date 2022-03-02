6c44a037c08de369792c53c09ac6a44164933bbd
Nathan Cohen <s015486@students.lmsd.org>
2022-03-02 14:30:34 -0500

    app colors

---
+++ MyGraine/Model/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MyGraine
+//
+//  Created by Nathan Cohen (student LM) on 3/2/22.
+//
+
+import SwiftUI
+
+extension Color{
+    static let backGround = Color("backGroundColor")
+    static let buttonBackground = Color("buttonBackGround")
+    static let buttonText = Color("buttonText")
+}
