4b0f1e853565b6929d17c240bcb716cea6c8de04
s016651 <s016651@students.lmsd.org>
2022-03-25 13:57:06 -0400

    Created new model file to hold migraine info

---
+++ MyGraine/Model/MigraineInfo.swift
@@ -0,0 +1,24 @@
+//
+//  MigraineInfo.swift
+//  MyGraine
+//
+//  Created by Adam Lee (student LM) on 3/25/22.
+//
+
+import SwiftUI
+
+class MigraineInfo : ObservableObject {
+    var date : String
+    var stress : String
+    var hydration : String
+    
+    init(date : String = "" , stress : String = "" , hydration : String = "") {
+        self.date = date
+        self.stress = stress
+        self.hydration = hydration
+        
+        
+        
+    }
+}
+
