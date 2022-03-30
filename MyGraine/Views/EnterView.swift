1d6668315efef5c62b37c550489cd81f38cf52ca
s016651 <s016651@students.lmsd.org>
2022-03-30 13:40:12 -0400

    started enter view

--- MyGraine/Views/EnterView.swift
+++ MyGraine/Views/EnterView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct EnterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle().foregroundColor(Color.backGroundColor)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("NEW MIGRAINE")
+                    .foregroundColor(Color.buttonText)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+        }
     }
 }
 
