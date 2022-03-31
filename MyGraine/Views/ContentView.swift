86173967440e50d22e8c9c60f0f04906bae88268
Nathan Cohen <s015486@students.lmsd.org>
2022-03-31 14:05:30 -0400

    implementing onboarding

--- MyGraine/Views/ContentView.swift
+++ MyGraine/Views/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         Group{
             if userInfo.loggedIn{
-                HomeView()
+                QuestionaireView()
             }
             else {
                 LoginView()
---
+++ MyGraine/Views/QuestionaireView.swift
@@ -0,0 +1,119 @@
+//
+//  QuestionaireView.swift
+//  MyGraine
+//
+//  Created by Nathan Cohen (student LM) on 3/31/22.
+//
+
+import SwiftUI
+
+struct QuestionaireView: View {
+    //    @AppStorage("shouldShowOnboarding") var shouldShowOnboarding: Bool = true
+        @State var shouldShowOnboarding: Bool = true
+        var body: some View {
+            NavigationView{
+             HomeView()
+                }
+            }.fullScreenCover(isPresented: $shouldShowOnboarding, content: {
+                OnboardingView(shouldShowOnboarding: $shouldShowOnboarding)
+            })
+        }
+    }
+
+    //Onboarding
+
+    struct OnboardingView: View {
+        @Binding var shouldShowOnboarding: Bool
+        var body: some View{
+            TabView{
+                YesNoView(title: "What Is Your Gender?", subtitle1: "Male", subtitle2: "Female", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                    
+                    .ignoresSafeArea(.all)
+                    
+                YesNoView(title: "have you been experiencing stress levels", subtitle1: "yes", subtitle2: "no",showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                    
+                YesNoView(title: "have you been drinking water", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                   
+              
+                YesNoView(title: "have you been experiencing constipation", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                
+                YesNoView(title: "have you experienced extreme mood changes or swings", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                
+                YesNoView(title: "have you been yawning frequently", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                
+                YesNoView(title: "have you been exepriencing vision loss or seeing bright spots", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                
+                YesNoView(title: "have you been having difficulty talking", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                
+                YesNoView(title: "have you been experiencing a numbness sensation on your face", subtitle1: "yes", subtitle2: "no", showsDismissButton: false, shouldShowOnboarding: $shouldShowOnboarding)
+                YesNoView(title: ""
+                          , subtitle1: ""
+                          , subtitle2: ""
+                          , showsDismissButton: true
+                          , shouldShowOnboarding: $shouldShowOnboarding)
+                   
+            }.tabViewStyle(PageTabViewStyle())
+        }
+    }
+    struct YesNoView: View {
+        let title: String
+        let subtitle1: String
+        let subtitle2: String
+        let showsDismissButton: Bool
+        @Binding var shouldShowOnboarding: Bool
+       
+        
+
+
+        var body: some View{
+            VStack{
+                Spacer()
+                Text(title)
+                    .font(.system(size: 42))
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text(subtitle1)
+                        .background(Color.white)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 20))
+                        .foregroundColor(Color(.secondaryLabel))
+                        .padding()
+                        .multilineTextAlignment(.center)
+                })
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text(subtitle2)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 20))
+                        .foregroundColor(Color(.secondaryLabel))
+                        .padding()
+                        .multilineTextAlignment(.center)
+                    
+                    
+                })
+                if showsDismissButton {
+                    Button(action: {
+                        shouldShowOnboarding.toggle()
+                    } , label: {
+                        Text("Get Started")
+                            .bold()
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .background(Color.green)
+                            .cornerRadius(6)
+                    })
+                }
+            
+               
+                Spacer()
+            }.ignoresSafeArea(.all)
+        }
+    }
+
+struct QuestionaireView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionaireView()
+    }
+}
